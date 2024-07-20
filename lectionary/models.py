0ff611b3d53967d61fbe55cc81fafa17ef25cc2f
William Wingate <wtwingate@gmail.com>
2024-07-20 15:41:11 -0400

    Start building models

--- lectionary/models.py
+++ lectionary/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Day(models.Model):
+    """A model representing a specific holy day within the three-year
+    liturgical calendar.
+    """
+
+    class Year(models.TextChoices):
+        A = ("A", "Year A")
+        B = ("B", "Year B")
+        C = ("C", "Year C")
+
+    class Season(models.TextChoices):
+        ADVENT = ("AD", "Advent")
+        CHRISTMAS = ("CH", "Christmas")
+        EPIPHANY = ("EP", "Epiphany")
+        LENT = ("LE", "Lent")
+        EASTER = ("EA", "Easter")
+        PENTECOST = ("PE", "Pentecost")
+
+    name = models.CharField(max_length=256)
+    alt_name = models.CharField(max_length=256, null=True, blank=True)
+    service = models.CharField(max_length=256, null=True, blank=True)
+    year = models.CharField(max_length=1, choices=Year)
+    season = models.CharField(max_length=2, choices=Season)
+    lessons = models.ManyToManyField("Lesson")
+
+    class Meta:
+        indexes = [models.Index(fields=["name"])]
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"pk": self.pk})
+
+
+class Lesson(models.Model):
+    """A model representing a specific lesson appointed in the
+    lectionary for one or more holy days. Note that the scripture field
+    may have alternate readings separated by " or ".
+    """
+
+    scripture = models.CharField(max_length=256, unique=True)
+    days = models.ManyToManyField("Day")
+
+    def __str__(self):
+        return f"{self.scripture}"

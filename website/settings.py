06bf7b365b506d18233c6034abe3c7560127b2c4
William Wingate <wtwingate@gmail.com>
2024-07-30 21:56:46 -0400

    Enable basic logging to the console

--- website/settings.py
+++ website/settings.py
@@ -130,3 +130,17 @@ CACHES = {
         "LOCATION": "cache_table",
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}

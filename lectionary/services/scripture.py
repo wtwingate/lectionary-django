a277b425f810e5e8877dd36754c5119e2e491e3f
William Wingate <wtwingate@gmail.com>
2024-08-16 18:08:47 -0400

    Use API.Bible for KJV and Apocryphal texts

--- lectionary/services/scripture.py
+++ lectionary/services/scripture.py
@@ -4,9 +4,11 @@ import re
 
 import requests
 
-ESV_HTML_URL = "https://api.esv.org/v3/passage/html/"
-ESV_TEXT_URL = "https://api.esv.org/v3/passage/text/"
+ESV_API_URL = "https://api.esv.org/"
 ESV_API_KEY = os.environ.get("ESV_API_KEY")
+BIBLE_API_URL = "https://api.scripture.api.bible/"
+BIBLE_API_KEY = os.environ.get("BIBLE_API_KEY")
+KJV_ID = "de4e12af7f28f599-01"
 TIMEOUT = 30
 
 logger = logging.getLogger("django")
@@ -20,7 +22,30 @@ def short_reference(reference: str) -> str:
     return re.sub(r"\(.*\),?\s*", "", reference)
 
 
+def get_kjv_html(reference: str) -> str:
+    url = f"{BIBLE_API_URL}v1/bibles/{KJV_ID}/search"
+
+    params = {
+        "query": long_reference(reference),
+        "sort": "relevance",
+    }
+
+    headers = {"api-key": BIBLE_API_KEY}
+
+    response = requests.get(url, params=params, headers=headers, timeout=30)
+
+    passages = "".join([p["content"] for p in response.json()["data"]["passages"]])
+
+    if passages:
+        return passages
+    else:
+        logger.error(f"Error: could not fetch {reference}.")
+        return ""
+
+
 def get_esv_html(reference: str) -> str:
+    url = f"{ESV_API_URL}/v3/passage/html/"
+
     params = {
         "q": long_reference(reference),
         "include-passage-references": False,
@@ -34,9 +59,7 @@ def get_esv_html(reference: str) -> str:
         "Authorization": f"Token {ESV_API_KEY}",
     }
 
-    response = requests.get(
-        ESV_HTML_URL, params=params, headers=headers, timeout=TIMEOUT
-    )
+    response = requests.get(url, params=params, headers=headers, timeout=TIMEOUT)
 
     passages = "".join(response.json()["passages"])
 
@@ -48,6 +71,8 @@ def get_esv_html(reference: str) -> str:
 
 
 def get_esv_text(reference: str) -> str:
+    url = f"{ESV_API_URL}/v3/passage/text/"
+
     params = {
         "q": long_reference(reference),
         "include-footnotes": False,
@@ -63,9 +88,7 @@ def get_esv_text(reference: str) -> str:
         "Authorization": f"Token {ESV_API_KEY}",
     }
 
-    response = requests.get(
-        ESV_TEXT_URL, params=params, headers=headers, timeout=TIMEOUT
-    )
+    response = requests.get(url, params=params, headers=headers, timeout=TIMEOUT)
 
     passages = "".join(response.json()["passages"])
 
